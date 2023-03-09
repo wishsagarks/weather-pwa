c080b81e4011310ee2ff898684e7c2c42164ad09
SAGAR SATAPATHY <wishsagarks@gmail.com>
2023-03-09 08:16:20 +0530

    build

---
+++ dist/precache-manifest.8001f9201299a1725230778f558b6941.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ede19b093d97208f5a4",
+    "url": "/progressive-weather-app/js/vendor.2ede19b0.js"
+  },
+  {
+    "revision": "dfdb119192d27e8a144f",
+    "url": "/progressive-weather-app/js/app.dfdb1191.js"
+  },
+  {
+    "revision": "dfdb119192d27e8a144f",
+    "url": "/progressive-weather-app/css/app.ca9b78dc.css"
+  },
+  {
+    "revision": "089cd8beba94851ed228",
+    "url": "/progressive-weather-app/js/manifest.089cd8be.js"
+  },
+  {
+    "revision": "242ca2f39c270713473fa8d2996ba35e",
+    "url": "/progressive-weather-app/img/night.242ca2f3.jpg"
+  },
+  {
+    "revision": "a81f0a400f3934c7d0d2dfbb791779b4",
+    "url": "/progressive-weather-app/img/morning.a81f0a40.jpg"
+  },
+  {
+    "revision": "70245bb9c386d443d8cf3b3a942f71f6",
+    "url": "/progressive-weather-app/img/humidity.70245bb9.svg"
+  },
+  {
+    "revision": "c13fda3eb3a2d7a982c45a729335eec4",
+    "url": "/progressive-weather-app/img/cloud.c13fda3e.svg"
+  },
+  {
+    "revision": "f5c5756d14b7215cb5ac27323f983cc8",
+    "url": "/progressive-weather-app/img/high.f5c5756d.svg"
+  },
+  {
+    "revision": "46fc3f95b365c885b57ae5cf314eca7e",
+    "url": "/progressive-weather-app/img/wind.46fc3f95.svg"
+  },
+  {
+    "revision": "987af2062c6a5a5ded5d595f4d39acd4",
+    "url": "/progressive-weather-app/img/low.987af206.svg"
+  },
+  {
+    "revision": "4628ea0b9daf3b3072b88a241b698663",
+    "url": "/progressive-weather-app/img/cloud.4628ea0b.svg"
+  },
+  {
+    "revision": "56c13b8a025ec8662e29102aab87359b",
+    "url": "/progressive-weather-app/img/thunderstorm.56c13b8a.svg"
+  },
+  {
+    "revision": "14f0c5161da7429285d1d57503628d64",
+    "url": "/progressive-weather-app/img/rain.14f0c516.svg"
+  },
+  {
+    "revision": "2c26e3748a334065e81866823644a148",
+    "url": "/progressive-weather-app/img/snow.2c26e374.svg"
+  },
+  {
+    "revision": "c5fecb1efbdb89698d14abf206a5f5a6",
+    "url": "/progressive-weather-app/index.html"
+  },
+  {
+    "revision": "ed5e368f0f9f950ec5e66e08205936f3",
+    "url": "/progressive-weather-app/img/site-image-lg.jpg"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -12,7 +12,7 @@
  */
 
 importScripts(
-  "/progressive-weather-app/precache-manifest.ab644820230c5211906cf74d93f4a4a3.js",
+  "/progressive-weather-app/precache-manifest.8001f9201299a1725230778f558b6941.js",
   "https://storage.googleapis.com/workbox-cdn/releases/3.0.0-beta.0/workbox-sw.js"
 );
 
